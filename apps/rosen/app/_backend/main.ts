bbea5f3ba71bce37ec562f152b1ee80558fca496
Mohammad Kermani <mkermani144@gmail.com>
2023-11-30 11:18:55 +0000

    gandekari 2

--- apps/rosen/app/_backend/main.ts
+++ apps/rosen/app/_backend/main.ts
@@ -1,10 +1,17 @@
 import './bootstrap';
 
+const x = path.resolve(
+  path.dirname(fileURLToPath(import.meta.url)),
+  '../../configs/tokensMap.json',
+);
+
 import WinstonLogger from '@rosen-bridge/winston-logger';
 
 import scannerService from './scanner/scanner-service';
 
 import dataSource from './dataSource';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const logger = WinstonLogger.getInstance().getLogger(import.meta.url);
 
