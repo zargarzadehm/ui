986c6ac5b4c3baf6912a4a241726d2c223862465
Mohammad Kermani <mkermani144@gmail.com>
2023-11-30 09:31:51 +0000

    test4

--- apps/rosen/instrumentation.ts
+++ apps/rosen/instrumentation.ts
@@ -1,10 +1,9 @@
-try {
-  console.log('!!!!!!!!!!!!!!!!! CWD in instrumentation: ', process.cwd);
-} catch (error) {
-  console.warn('Ye etefaghe dige', error);
-}
-
 export const register = async () => {
+  try {
+    console.log('!!!!!!!!!!!!!!!!! CWD in instrumentation: ', process.cwd);
+  } catch (error) {
+    console.warn('Ye etefaghe dige', error);
+  }
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     import('./app/_backend/main');
   }
