cd120e0b98d93358c5a5c0d199eb00a54680dd86
Mohammad Kermani <mkermani144@gmail.com>
2023-12-02 10:57:53 +0000

    timeout

--- apps/rosen/app/_backend/constants.ts
+++ apps/rosen/app/_backend/constants.ts
@@ -1,7 +1,7 @@
 export const ERGO_EXPLORER_URL = 'https://api.ergoplatform.com';
 export const CARDANO_KOIOS_URL = 'https://api.koios.rest/api/v1';
 
-export const SCANNER_API_TIMEOUT = 10_000;
+export const SCANNER_API_TIMEOUT = 30_000;
 
 export const ERGO_SCANNER_INTERVAL = 2 * 60 * 1000;
 export const CARDANO_SCANNER_INTERVAL = 30 * 1000;
